d5ddff46fece22b6395422ccf89101bb0f693967
Cherkesky <guy@cherkesky.com>
2020-04-27 11:17:03 -0500

    starting building Randals data types

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿  
+using System;
+using System.Collections.Generic;
+
+namespace RandallsCarLot
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+    //  vehicles  each sale    each vehicle           make      subaru
+        List<Dictionary<string, Dictionary<string, String>>> vehicles = new List <Dictionary<string, Dictionary<string, String>>>();
+
+        Dictionary<string, Dictionary<string, string>> sale1 = new Dictionary<string, Dictionary<string, string>>();
+
+        Dictionary<string, string> vehicle = new Dictionary<string, string>();
+    
+        Dictionary<string, string> salesAgent = new Dictionary<string, string>();
+
+        Dictionary<string, string> credit = new Dictionary<string, string>();
+
+        vehicle.Add("year","2008");
+        vehicle.Add("model","Damfresh");
+        vehicle.Add("make","Biotraxquote");
+        vehicle.Add("color","sky magenta");
+
+
+
+        }
+    }
+}
