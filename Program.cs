f0a07c1f38be67c67ae9f5a73ab67a8334ff84de
Cherkesky <guy@cherkesky.com>
2020-04-27 11:27:42 -0500

    added some data to the data structures

--- Program.cs
+++ Program.cs
@@ -24,8 +24,46 @@ namespace RandallsCarLot
         vehicle.Add("make","Biotraxquote");
         vehicle.Add("color","sky magenta");
 
+        salesAgent.Add("mobile", "896 478-6975");
+        salesAgent.Add("lastName", "Botsford");
+        salesAgent.Add("firstName", "Shaina");
+
+        credit.Add("creditProvider", "JP Morgan Chase");
+        credit.Add("account", "601109582720302");
+
+        sale1.Add("vehicle",vehicle);
+        sale1.Add("sales_agent", salesAgent);
+        sale1.Add("credit", credit);
+
+        vehicles.Add(sale1);
+
+        Dictionary<string, Dictionary<string, string>> sale2 = new Dictionary<string, Dictionary<string, string>>();
+
+        Dictionary<string, string> vehicle2 = new Dictionary<string, string>();
+    
+        Dictionary<string, string> salesAgent2 = new Dictionary<string, string>();
+
+        Dictionary<string, string> credit2 = new Dictionary<string, string>();
+
+        vehicle2.Add("year","2008");
+        vehicle2.Add("model","Damfresh");
+        vehicle2.Add("make","Biotraxquote");
+        vehicle2.Add("color","sky magenta");
+
+        salesAgent2.Add("mobile", "896 478-6975");
+        salesAgent2.Add("lastName", "Botsford");
+        salesAgent2.Add("firstName", "Shaina");
+
+        credit2.Add("creditProvider", "JP Morgan Chase");
+        credit2.Add("account", "601109582720302");
+
+        sale2.Add("vehicle",vehicle);
+        sale2.Add("sales_agent", salesAgent);
+        sale2.Add("credit", credit);
+
+        vehicles.Add(sale2);
 
 
-        }
+       }
     }
 }
